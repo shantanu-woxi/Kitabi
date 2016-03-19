e7ce9bc7bf5cd6d1cea0079bbd9b27278f39ad28
Shantanu Acharya <shantanu.woxi@gmail.com>
2016-03-19 21:52:02 +0530

    stableized: Shantanu

--- application/models/User.php
+++ application/models/User.php
@@ -17,11 +17,11 @@ class User extends CI_Model
             $result=$this->db->query($query);
             foreach ($result->result() as $x)
                 {
-                        $userData=array('id'=>$x->id,'fname'=>$x->first_name , 'lname'=> $x->last_name, 'role'=>$x->role, 'verified'=>$x->verified);
-                        $this->load->library('session');
-                        $this->load->helper('url');
-                        $this->session->set_userdata($userData);
-                        $id=$this->session->userdata('id');
+                    $userData=array('id'=>$x->id,'fname'=>$x->first_name , 'lname'=> $x->last_name, 'role'=>$x->role);
+                    $this->load->library('session');
+                    $this->load->helper('url');
+                    $this->session->set_userdata($userData);
+                    $id=$this->session->userdata('id');
                 }
             if($result->num_rows()>0)
             {
